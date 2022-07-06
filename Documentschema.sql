04654f581d2b336827aac90dc039aa2cf2222431
lady-jessicah <chizobajessica55@gmail.com>
2022-07-06 17:06:58 +0100

    additional files

--- Documentschema.sql
+++ Documentschema.sql
@@ -1,6 +1,15 @@
 CREATE TABLE Person
-PersonID (auto integer)
-name varchar(255)
-description longtext
-status boolean
-created_at (timestamp)
+	(
+    PersonID int,
+	name varchar(255),
+	description longtext,
+	status boolean,
+	created_at timestamp
+    );
+INSERT INTO Person (PersonID, name, description, status, created_at)
+VALUES ('6', 'Udechukwu Jessica', 'consultant at Deelaa', '1', CURRENT_TIMESTAMP);
+SET SQL_SAFE_UPDATES = 0;
+UPDATE Person SET description = 'Tech-bro' WHERE PersonID = 1;
+DELETE FROM Person WHERE PersonID = 1;
+DELETE FROM Person;
+DROP DATABASE babysteps2sql
